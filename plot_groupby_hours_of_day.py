fb906c6428af6877e3b40feee739193a111f522a
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2020-08-18 09:43:29 -0300

    plot by hour of day normalized

--- plot_groupby_hours_of_day.py
+++ plot_groupby_hours_of_day.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 tab = Table()
-tab.setCons_kW()
-tab.setInjection_kWp(0.5)
+# tab.setCons_kW()
+# tab.setInjection_kWp(1.5)
+
+year = tab.getTable()
 
 year.groupby(year['Datetime'].dt.hour).sum().plot(kind='bar')
-
+plt.legend(['Consumption', 'Production'])
+plt.xlabel('Hours')
+# plt.ylabel('kWh')
 plt.show()
